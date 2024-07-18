46c9aa8bc58f71fb32dfaa295dff2a02be583521
Mathieu <m.hembert@outlook.fr>
2024-07-18 13:14:05 +0200

    commit routes midi

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-=======
 const Trend = require('../models/trends')
 const Tweet = require('../models/tweets')
 
@@ -14,7 +12,6 @@ router.post('/', (req, res) =>{
 
 //GET’/’ (trend)  —> {tweets}
 
->>>>>>> 4193f14a9fc14df2a2a128af69373f64cc3b0370
 
 
 module.exports = router;
