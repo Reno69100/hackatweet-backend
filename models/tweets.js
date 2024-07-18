b7874fcc0d424281615f773d7b1cfbb07af14c08
Renaud69100 <r.akehal@gmail.com>
2024-07-18 12:21:37 +0200

    creation du modèle tweets

---
+++ models/tweets.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const trendSchema = mongoose.Schema({
+    user:{ type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    date: Date,
+    message: String,
+    nbLike: Number,
+    trends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'trends' }],
+    
+
+});
+
+const Trend = mongoose.model('Trend', trendSchema);
