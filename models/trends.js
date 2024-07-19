094e1648d6061b1b3f09fab9a5a66a96779a4f30
Mathieu <m.hembert@outlook.fr>
2024-07-19 13:04:29 +0200

    Modification model et route trend

--- models/trends.js
+++ models/trends.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const trendSchema = mongoose.Schema({
     name:String,
+    nbTweets: Number,
 })
 
 const Trend = mongoose.model('trends', trendSchema);
--- routes/trends.js
+++ routes/trends.js
@@ -5,27 +5,34 @@ const Trend = require('../models/trends')
 //get trends
 router.get("/", (req, res) => {
   Trend.find()
-  .then((data)=>{
-    console.log(data)
-    res.json({result:true, trends:data})
-  })
+    .then((data) => {
+      console.log(data)
+      res.json({ result: true, trends: data })
+    })
 });
 
 //add trend
-router.post("/:name", (req, res) =>{
-  Trend.findOne({name:req.params.name})
-  .then((data)=>{
-    if(!data){
-        const newTrend = new Trend({name: req.params.name})
-        newTrend.save()
-        .then((data)=>{
-          console.log(data)
-          res.json({result:true})
+router.post("/:name", (req, res) => {
+  Trend.findOne({ name: req.params.name })
+    .then((data) => {
+      if (!data) {
+        const newTrend = new Trend({
+          name: req.params.name,
+          nbTweets: 1,
         })
-  }else{
-    res.json({result:false, error:'already exists'})
-    }
-  })
+        newTrend.save()
+          .then((data) => {
+            console.log(data)
+            res.json({ result: true })
+          })
+      }
+      else {
+        Trend.updateOne({ name: req.params.name }, { $inc: { nbTweets: +1 } })
+        .then((data) => {
+          res.json({ result: true })
+        })
+      }
+    })
 })
 
 module.exports = router;
